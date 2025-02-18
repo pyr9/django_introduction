25b290e078e4b1637df98f71bcfde91ade6f1afb
panyuro <panyuro@163.com>
2025-02-18 11:47:02 +0800

    实现博客数据返回界面
    visit `http://127.0.0.1:8000/blog/article`

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 import blog.views
 
 urlpatterns = [
-    path('hello_world', blog.views.hello_world)
+    path('hello_world', blog.views.hello_world),
+    path('article', blog.views.article_content)
 ]
--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,17 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+from blog.models import Article
+
+
 def hello_world(request):
     return HttpResponse("Hello, world. This is first page.")
 
+def article_content(request):
+    article = Article.objects.all()[0]
+    title = article.title
+    content = article.content
+    id = article.id
+    pub_date = article.pub_date
+    return_str = 'id: %s, title: %s, content: %s, pub_date: %s' % (id, title, content, pub_date)
+    return HttpResponse(return_str)
