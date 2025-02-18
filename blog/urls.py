6de0d6d89c2a0cf8c1cede1556bfa46d16475271
panyuro <panyuro@163.com>
2025-02-18 16:22:28 +0800

    博客系统基础功能开发
    1. 博客列表
    2. 详情界面
    3. 上下篇文章跳转
    4. 实现分页功能
    5. 实现最近文章列表

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,7 @@ import blog.views
 
 urlpatterns = [
     path('hello_world', blog.views.hello_world),
-    path('article', blog.views.article_content)
+    path('article', blog.views.article_content),
+    path('index', blog.views.get_index_page),
+    path('detail/<int:id>', blog.views.get_detail_page)
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.http import HttpResponse
 from django.shortcuts import render
 
@@ -16,6 +17,80 @@ def article_content(request):
     title = article.title
     content = article.content
     id = article.id
-    pub_date = article.pub_date
-    return_str = 'id: %s, title: %s, content: %s, pub_date: %s' % (id, title, content, pub_date)
+    publish_date = article.pub_date
+    return_str = 'title: %s, ' \
+                 'content: %s, id: %s, publish_date: %s' % (title,
+                                                                    content,
+                                                                    id,
+                                                                    publish_date)
     return HttpResponse(return_str)
+
+
+def get_index_page(request):
+    page = request.GET.get('page')
+    if page:
+        page = int(page)
+    else:
+        page = 1
+    print('page param: ', page)
+
+    all_article = Article.objects.all()
+    top10_article_list = Article.objects.order_by('-pub_date')[:10]
+
+    paginator = Paginator(all_article, 6)
+    page_num = paginator.num_pages
+    print('page num:', page_num)
+    page_article_list = paginator.page(page)
+    if page_article_list.has_next():
+        next_page = page + 1
+    else:
+        next_page = page
+    if page_article_list.has_previous():
+        previous_page = page - 1
+    else:
+        previous_page = page
+
+    return render(request, 'blog/index.html',
+                  {
+                      'article_list': page_article_list,
+                      'page_num': range(1, page_num + 1),
+                      'curr_page': page,
+                      'next_page': next_page,
+                      'previous_page': previous_page,
+                      'top10_article_list': top10_article_list
+                  }
+                )
+
+
+def get_detail_page(request, id):
+    all_article = Article.objects.all()
+    curr_article = None
+    previous_index = 0
+    next_index = 0
+    previous_article = None
+    next_article = None
+    for index, article in enumerate(all_article):
+        if index == 0:
+            previous_index = 0
+            next_index = index + 1
+        elif index == len(all_article) - 1:
+            previous_index = index - 1
+            next_index = index
+        else:
+            previous_index = index - 1
+            next_index = index + 1
+        if article.id == id:
+            curr_article = article
+            previous_article = all_article[previous_index]
+            next_article = all_article[next_index]
+            break
+
+    section_list = curr_article.content.split('\n')
+    return render(request, 'blog/detail.html',
+                  {
+                      'curr_article': curr_article,
+                      'section_list': section_list,
+                      'previous_article': previous_article,
+                      'next_article': next_article
+                  }
+                  )
