fb504e0fe8329ba2dc2f022d8c7e7ab1db8ac790
panyuro <panyuro@163.com>
2025-02-18 10:14:11 +0800

    create article model
    1. update models.py
    2. run `python manage.py` makemigrations
     -> generate 0001_initial.py
    3. run `python manage.py migrate` -> update db.sqlite3

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+class Article(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.TextField()
+    content = models.TextField()
+    pub_date = models.DateTimeField(auto_now=True)
